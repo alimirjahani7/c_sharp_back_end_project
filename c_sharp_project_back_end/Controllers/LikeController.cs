1fe3768cbddbd40a4e7850b325341c502125e544
Parmiss <parmiss.sh98@gmail.com>
2019-01-30 17:53:18 +0330

    like works

--- c_sharp_project_back_end/Controllers/LikeController.cs
+++ c_sharp_project_back_end/Controllers/LikeController.cs
@@ -17,18 +17,36 @@ namespace c_sharp_project_back_end.Controllers
         {
             context = _context;
         }
-
         // POST api/like/numberOflikes
         [HttpGet("numberOfLikes")]
         public void Post(int PostId)
         {
 
         }
-
-        // Post api/likeUnlike
-        [HttpGet("like")]
-        public void Put(int PostId,int UserId)
+        [HttpGet("{id}/likes")]
+        public async Task<ActionResult<IEnumerable<Like>>> GetPostLikes(int id)
         {
+            return context.Likes.Where(x => x.PostId == id).ToList();
+        }
+
+        // post api/like
+        [HttpPost]
+        public async Task<ActionResult<Like>> LikePost(Like like)
+        {
+            var like2 = context.Likes.Where(x => x.PostId == like.PostId).Where(x => x.UserId == like.UserId);
+            if (like2.ToList().Count != 0)
+            {
+                System.Diagnostics.Debug.WriteLine("goddamn " + like2 + " goddamn");
+
+                foreach (var item in like2)
+                    context.Likes.Remove(item);
+            }
+            else
+            {
+                context.Likes.Add(like);
+            }
+            await context.SaveChangesAsync();
+            return like;
 
         }
 
